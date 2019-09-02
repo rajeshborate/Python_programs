d9032bfbd548434046889fbb660a2cf50657cb31
Rajesh Borate <rajeshborate08@gmail.com>
2019-09-02 20:07:56 +0530

    Update Turtle_first_program.py

--- Turtle_first_program.py
+++ Turtle_first_program.py
@@ -4,9 +4,13 @@ Created on Mon Sep  2 19:59:39 2019
 
 @author: Rajesh D Borate
 """
+# first import turtle library
 import turtle
+# create a screen for turtle to move
 sc=turtle.Screen()
+# create a turtle named alpha
 alpha=turtle.Turtle()
+#the below codes are the movement of the turtle
 alpha.forward(200)
 alpha.left(90)
 alpha.forward(100)
