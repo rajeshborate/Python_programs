b0f20764d2e16f0affdd10f5817cd37019b73a53
Rajesh Borate <rajeshborate08@gmail.com>
2019-09-02 20:04:19 +0530

    Add files via upload

---
+++ Turtle_first_program.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 19:59:39 2019
+
+@author: Rajesh D Borate
+"""
+import turtle
+sc=turtle.Screen()
+alpha=turtle.Turtle()
+alpha.forward(200)
+alpha.left(90)
+alpha.forward(100)
+alpha.left(90)
+alpha.forward(200)
+alpha.left(90)
+alpha.forward(100)
